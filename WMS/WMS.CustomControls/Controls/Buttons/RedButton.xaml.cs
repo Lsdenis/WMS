dfb8c6be2e3dac0b0aa30ed4ab77baed12bc7923
Denis P. <lsden1995@gmail.com>
2014-04-20 18:09:53 +0300

    Change custom buttons

---
+++ WMS/WMS.CustomControls/Controls/Buttons/RedButton.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WMS.CustomControls.Controls.Buttons
+{
+    /// <summary>
+    /// Interaction logic for BlueButton.xaml
+    /// </summary>
+    public partial class RedButton : UserControl
+    {
+        public string Text
+        {
+            set { btnRedButton.Content = value; }
+        }
+
+        public RoutedEventHandler Click
+        {
+            set { btnRedButton.Click += value; }
+
+        }
+
+        public RedButton()
+        {
+            InitializeComponent();
+        }
+    }
+}
